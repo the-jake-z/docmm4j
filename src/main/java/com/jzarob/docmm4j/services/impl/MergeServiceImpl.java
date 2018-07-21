2324ccd2c8bd7e5ac781462d639b1f0e54139e57
Jake Zarobsky <jzarob@gmail.com>
2018-07-21 10:19:51 -0400

    Added multiple merge functions

--- src/main/java/com/jzarob/docmm4j/services/impl/MergeServiceImpl.java
+++ src/main/java/com/jzarob/docmm4j/services/impl/MergeServiceImpl.java
@@ -1,20 +1,20 @@
 package com.jzarob.docmm4j.services.impl;
 
-import com.google.common.io.Files;
 import com.jayway.jsonpath.Configuration;
 import com.jayway.jsonpath.JsonPath;
+import com.jzarob.docmm4j.exceptions.MultiMergeException;
 import com.jzarob.docmm4j.models.*;
 import com.jzarob.docmm4j.services.DocumentService;
 import com.jzarob.docmm4j.services.MergeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.security.acl.Group;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.ZipEntry;
@@ -55,79 +55,86 @@ public class MergeServiceImpl implements MergeService {
         );
     }
 
-    private static Map<String, String> getMergeFieldValues(Map<String, String> mappingScheme, Object mergeObjectData) {
+    private static Map<String, String> getMergeFieldValues(Map<String, String> mappingScheme, Object jsonObjectData) {
         Map<String, String> mergeFieldValues = new HashMap<>();
         mappingScheme.forEach((key, value) -> {
-            mergeFieldValues.put(key, JsonPath.read(mergeObjectData, value));
+            mergeFieldValues.put(key, JsonPath.read(jsonObjectData, value));
         });
 
         return mergeFieldValues;
     }
 
+    private void mergeGroupInDirectory(Groupings groupings, Path rootDirectory) throws IOException {
+        Path groupDirectory = rootDirectory.resolve(groupings.getName());
+        Files.createDirectories(groupDirectory);
+
+        for(MergeTemplateRequest request : groupings.getMergeRequests()) {
+            mergeDocumentInDirectory(request, groupDirectory);
+        }
+    }
+
+    private File createFileInDirectory(String fileName, Path directory) throws IOException {
+        Path filePath = directory.resolve(fileName);
+        Files.createFile(filePath);
+        return filePath.toFile();
+    }
+
+    private void mergeDocumentInDirectory(MergeTemplateRequest request, Path groupDirectory) throws IOException {
+        Document document = documentService.loadByDocumentNumber(request.getDocumentNumber());
+        Object jsonObjectData = Configuration
+                .defaultConfiguration()
+                .jsonProvider()
+                .parse(request.getMergeData());
+
+        Map<String, String> mergeFieldValues = getMergeFieldValues(document.getMappingScheme(), jsonObjectData);
+
+        FileOutputStream fileOutputStream = new FileOutputStream(
+                createFileInDirectory(document.getFileName(), groupDirectory));
+
+        mergeDocument(document.getFormTemplateInputStream(), mergeFieldValues, fileOutputStream);
+    }
+
     @Override
-    // TODO: major refactor. Don't let uncle bob see this one kids.
     public Resource mergeMultipleDocuments(MultiMergeRequest multiMergeRequest) {
 
         try {
-            File rootDirectory = new File(Paths.get(Files.createTempDir().getPath().toString(),
-                    "batch").toString());
-
-            rootDirectory.mkdir();
+            Path rootDirectory = Files.createTempDirectory("batch");
 
             for(Groupings group : multiMergeRequest.getGroupings()) {
-                File groupDirectory = new File(Paths.get(rootDirectory.getPath(), group.getName()).toString());
-                groupDirectory.mkdir();
-
-                for(MergeTemplateRequest request : group.getMergeRequests()) {
-                    Document d = documentService.loadByDocumentNumber(request.getDocumentNumber());
-                    Object mergeDataObject = Configuration.defaultConfiguration().jsonProvider()
-                            .parse(request.getMergeData());
-
-                    Map<String, String> mergeFieldValues = getMergeFieldValues(d.getMappingScheme(), mergeDataObject);
-
-                    File file = new File(Paths.get(groupDirectory.getPath().toString(), d.getFileName()).toString());
-                    file.createNewFile();
-
-                    FileOutputStream fileOutputStream = new FileOutputStream(
-                            Paths.get(groupDirectory.getPath().toString(),
-                                      d.getFileName()).toString()
-                    );
-
-                    mergeDocument(d.getFormTemplateInputStream(), mergeFieldValues, fileOutputStream);
-                }
+                mergeGroupInDirectory(group, rootDirectory);
             }
 
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            Path temporaryZipFile = Files.createTempFile(null, ".zip");
+            FileOutputStream zipFileOutputStream =
+                    new FileOutputStream(temporaryZipFile.toString());
 
-            pack(rootDirectory.getPath(), outputStream);
 
-            return new ByteArrayResource(outputStream.toByteArray());
+            zipDirectory(rootDirectory, zipFileOutputStream);
 
-        } catch (Exception ex) {
-            System.out.println("some exception");
+            return new FileSystemResource(temporaryZipFile.toFile());
+
+        } catch (IOException ex) {
+            throw new MultiMergeException(ex);
         }
-
-        return null;
     }
 
 
     // Shamelessly stolen from:
     // https://stackoverflow.com/questions/15968883/how-to-zip-a-folder-itself-using-java
-    public static void pack(String sourceDirPath, OutputStream outputStream) throws IOException {
+    private static void zipDirectory(Path sourceDirPath, OutputStream outputStream) throws IOException {
         try (ZipOutputStream zs = new ZipOutputStream(outputStream)) {
-            Path pp = Paths.get(sourceDirPath);
-            java.nio.file.Files.walk(pp)
-                    .filter(path -> !java.nio.file.Files.isDirectory(path))
-                    .forEach(path -> {
-                        ZipEntry zipEntry = new ZipEntry(pp.relativize(path).toString());
-                        try {
-                            zs.putNextEntry(zipEntry);
-                            java.nio.file.Files.copy(path, zs);
-                            zs.closeEntry();
-                        } catch (IOException e) {
-                            System.err.println(e);
-                        }
-                    });
+            Files.walk(sourceDirPath)
+                .filter(path -> !java.nio.file.Files.isDirectory(path))
+                .forEach(path -> {
+                    ZipEntry zipEntry = new ZipEntry(sourceDirPath.relativize(path).toString());
+                    try {
+                        zs.putNextEntry(zipEntry);
+                        java.nio.file.Files.copy(path, zs);
+                        zs.closeEntry();
+                    } catch (IOException e) {
+                        System.err.println(e);
+                    }
+                });
         }
     }
 }
